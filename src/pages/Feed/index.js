1662d5cf1c8989d8d171a71b6df1373251e75dc8
Carlos <carlos@iMac-de-Carlos-2.local>
2019-09-10 15:02:35 -0300

    adicionada-funcao-loading-e-scroll

--- src/pages/Feed/index.js
+++ src/pages/Feed/index.js
@@ -1,21 +1,40 @@
 import React, {useState, useEffect} from 'react';
 import {View, FlatList} from 'react-native';
-import {Post, Header, Avatar, Name, PostImage, Description} from './styles';
+import {
+  Post,
+  Header,
+  Avatar,
+  Name,
+  PostImage,
+  Description,
+  Loading,
+} from './styles';
 export default function Feed() {
   const [feed, setFeed] = useState([]);
+  const [page, setPage] = useState(1);
+  const [total, setTotal] = useState(1);
+  const [loading, setLoading] = useState(false);
+
+  async function loadPage(pageNumber = page) {
+    if (total && pageNumber > total) return;
+
+    setLoading(true);
+
+    const response = await fetch(
+      `http://localhost:3000/feed?_expand=author&_limit=5&_page=${pageNumber}`,
+    );
+
+    const data = await response.json();
+    const totalItems = response.headers.get('X-Total-Count');
+
+    setTotal(Math.floor(totalItems / 5));
+    setFeed([...feed, ...data]);
+    setPage(pageNumber + 1);
+    setLoading(false);
+  }
 
   useEffect(() => {
-    async function loadFeed() {
-      const response = await fetch(
-        'http://localhost:3000/feed?_expand=author&_limit=5&_page=1',
-      );
-
-      const data = await response.json();
-
-      setFeed(data);
-    }
-
-    loadFeed();
+    loadPage();
   }, []);
 
   return (
@@ -23,6 +42,9 @@ export default function Feed() {
       <FlatList
         data={feed}
         keyExtractor={post => String(post.id)}
+        onEndReached={() => loadPage()}
+        onEndReachedThreshold={0.1}
+        ListFooterComponent={loading && <Loading />}
         renderItem={({item}) => (
           <Post>
             <Header>
